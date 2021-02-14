ed12e9ef46f04a177c52ca9d6eb9a67b1077b194
cyyhexo <cyyhexo@qq.com>
2021-02-14 22:15:23 +0800

    Site updated: 2021-02-14 22:15:22

---
+++ js/buttom.js
@@ -0,0 +1,9 @@
+var tool = new xkTool(
+  "https://cdn.jsdelivr.net/gh/blogimg/picbed@master/2020/04/11/d5083a3e52ebc4e84520507492b41920.png",true
+);
+
+tool.consoleAnchor();
+
+
+tool.footFish();
+
